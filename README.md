66039598c7698495ac31fccc302e37a058250631
nephlm <git@quasisemi.com>
2016-05-25 12:57:50 -0400

    clean up and documentation

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
 # droute53
-dynamic dns using route53
+
+Dynamic DNS using route53.
+
+Elastic IP addresses can be associated with EC2 instances and that
+can be used to effectively solve the problem of attaching a name to
+an ephemeral resource.
+
+However AWS charges for Elastic IP addresses associated with stopped
+instances, rendering this inappropriate low budget operations or if
+you have many resourced kept primarily in a stopped state.
+
+This command line python script which should be appropriate for
+use by shell scripts automates looking up an instance and updating
+route53 with it's public facing IP address.
+
+It can also be run on any node with a public facing IP address and
+it can update route53 with public facing IP address.
+
+And just for fun it also has options to start or stop EC2 instances
+by name (or id).
+
+The script is based on boto2 so keys should be stored in a
+~/.boto configuration file.
+See http://boto.cloudhackers.com/en/latest/boto_config_tut.html
+for all the details.
+
+Basic usage for updating a host ``foo.example.com`` to point to
+public IP address of the EC2 instance with a name of ``web``.
+
+.. sourcecode:: sh
+
+    $ ./droute53.py foo.example.com web
+    IP Address found: 54.210.124.178
+    Found record:  A foo.example.com. ['71.163.166.62']
+    Updating to record:  A foo.example.com. 54.210.124.178
+
+If there is no record for foo.example.com but there is a route53
+HostedZone for ``example.com`` it will create a recored for
+``foo.example.com``
+
+The optional parameter ``--start`` will start a stopped instance and
+wait until the instance comes up before updating route53.
+
+The optional parameter ``--kill`` will stop a running instance and
+wait until the instance goes down.  With ``--kill`` the script does
+no updating of route53.
+
+The optional parameter ``--region`` allows the specification of the
+AWS region (``us-west-1``, etc.)  route53 doesn't use it, but it is
+required to find the EC2 instance.  By default the script checks
+``us-east-1`` (N. Virginia).
+
+If instead you want to update route 53 with the public IP address of
+localhost, simple leave off the instance name.
+
+.. sourcecode:: sh
+
+    $ ./droute53.py foo.example.com
+    No instance provided, looking up public IP.
+    IP Address found: 72.163.166.62
+    Found record:  A foo.example.com. ['54.210.124.178']
+    Updating to record:  A foo.example.com. 72.163.166.62
+
+If you include ``--start`` or ``--kill`` the script will exit with
+an error message, since those require that the script do something it
+can't do since no instance has been specified. ``--region`` is silently
+ignored since it doesn't change behavior.
+
+The script has only really been tested on my rather simple route53
+zone.  While I think it will work under more complicated situations,
+without testing I can't be sure.
+
+At present the script only manipulates ``A`` records though it
+shouldn't be too tough to expand that to do other sorts of
+DNS records.
+
+An example using the instance id and ``--start`` option.
+
+.. sourcecode:: sh
+
+    $ ./droute53.py foo.example.com i-326c87a2 --start
+    Starting web(i-326c87a2).
+    still waiting for operation on "i-326c87a2" to complete
+    still waiting for operation on "i-326c87a2" to complete
+    IP Address found: 54.174.46.34
+    Found record:  A foo.example.com. ['54.210.124.178']
+    Updating to record:  A foo.example.com. 54.174.46.34
+
+That's probably enough for a help file that probably nobody
+else in the whole world will read.
--- src/droute53.py
+++ src/droute53.py
@@ -17,17 +17,17 @@ import requests
 RUNNING = 16
 STOPPED = 80
 
-# HOST = 'demondice.quasisemi.com'
-# if HOST[-1] != '.':
-#     HOST += '.'
-# INSTANCE = 'web'
-# REGION = 'us-east-1'
-
 def getZone(r53Conn, host):
     """
-    return zone which might contain the specified host.
+    Return route53 zone which might contain the specified host.
+
     @param host: str -- fqdn for host including terminal '.'
-    @returns: boto route53 zone object.
+
+    @returns: tuple of boto route53 zone object (or None) and
+            resouceRecord (or None ``(zone, resource)`` or
+            ``(zone, None)`` or ``(None, None)``.  If an existing
+            record could be found it will be returned as the
+            second element.
     """
     candidates = []
     match = None
@@ -37,46 +37,107 @@ def getZone(r53Conn, host):
             if match:
                 # print(match.__dict__)
                 print('Found record:  A {} {}'.format(host, match.resource_records))
-                return zone
+                return (zone, match)
             candidates.append(zone)
     if not candidates:
-        return None
+        return (None, None)
     elif len(candidates) == 1:
-        return candidates[0]
+        return (candidates[0], None)
     else:
         # TODO: something more clever
-        return candidates[0]
+        return (candidates[0], None)
 
 def getInstance(ec2Conn, instance):
+    """
+    Return the boto instance object with a name or id matching instance.
+
+    @param ec2Conn: ec2 connection object to appropriate region
+    @param insance: str -- name or id of desired instance.
+
+    @return: boto ec2.Instance object.
+    """
     for instanceObj in ec2Conn.get_only_instances():
         if instanceObj.tags['Name'] == instance or instanceObj.id == instance:
             return instanceObj
+    print('No instance \'{}\' could be found.'.format(instance))
+    sys.exit(2)
+
 
 def getInstanceIPAddress(ec2Conn, instance):
-    instanceObj = getInstance(ec2Conn, instance)
-    if instanceObj:
-        return instanceObj.ip_address
-    else:
-        print('No instance \'{}\' could be found.'.format(instance))
-        sys.exit(2)
+    """
+    Return the public IP associated with the instance.
+
+    @param ec2Conn: ec2 connection object to appropriate region
+    @param insance: str -- name or id of desired instance.
+
+    @return: str -- public IP address.
+    """
+    return  getInstance(ec2Conn, instance).ip_address
+
+def changeState(instanceObj, fx, desiredState, opStr):
+    """
+    Change the state of the instance.
+
+    @param insanceObj: ec2.Instance -- Instance to be manipulated.
+    @param fx: function -- function to execute to change state.  Takes
+                no argument (e.g instanceObj.stop or instanceObj.start)
+    @param desiredState: int -- The state to be achieved.
+    @param opStr: str -- A string to reprsnting the operation to the,
+                user (e.g. 'Starting' or 'Stopping')
+
+    @return: None
+    """
+    # If it is changing state wait until the operation is complete.
+    waitForState(instanceObj, [RUNNING, STOPPED])
+    if instanceObj.state_code != desiredState:
+        try:
+            ident = instanceObj.tags['Name']
+        except KeyError:
+            ident = 'unknown'
+        print('{} {}({}).'.format(opStr, ident, instanceObj.id))
+        fx()
+        waitForState(instanceObj, [desiredState])
 
 def startInstance(ec2Conn, instance):
+    """
+    Start the instance if stopped.  Will exit if the instance can't
+    be found.
+
+    @param ec2Conn: ec2 connection object to appropriate region
+    @param insance: str -- name or id of the instance to manipulate.
+
+    @return: None
+    """
     instanceObj = getInstance(ec2Conn, instance)
-    print('Starting {}({}).'.format(instance, instanceObj.id))
-    if instanceObj.state_code == STOPPED:
-        instanceObj.start()
-    waitForState(instanceObj, RUNNING)
+    changeState(instanceObj, instanceObj.start, RUNNING, 'Starting')
 
 def killInstance(ec2Conn, instance):
+    """
+    Stop the instance if running.  Will exit if the instance can't
+    be found.
+
+    @param ec2Conn: ec2 connection object to appropriate region
+    @param insance: str -- name or id of instance to manipulate.
+
+    @return: None
+    """
     instanceObj = getInstance(ec2Conn, instance)
-    print('Stopping {}({}).'.format(instance, instanceObj.id))
-    if instanceObj.state_code == RUNNING:
-        instanceObj.stop()
-    waitForState(instanceObj, STOPPED)
+    changeState(instanceObj, instanceObj.stop, STOPPED, 'Stopping')
 
 def waitForState(instanceObj, state):
+    """
+    Wait until the instance is in a desired state.  This function
+    doesn't initiate any change, so if not used carefully it may wait
+    forever.
+
+    @param insanceObj: ec2.Instance -- Instance to be manipulated.
+    @param state: list of int -- List of acceptable states.
+
+    @returns: None
+
+    """
     cnt = 0
-    while instanceObj.state_code != state:
+    while instanceObj.state_code not in state:
         time.sleep(1)
         instanceObj.update()
         cnt +=1
@@ -84,16 +145,32 @@ def waitForState(instanceObj, state):
             print('still waiting for operation on "{}" to complete'.format(instanceObj.id))
 
 def getMyIPAddress():
+    """
+    Returns the public facing IP address of localhost.  Depending on
+    network engineering there may be no way to reach localhost by
+    using this address, but it will certainly be where traffic from
+    localhost will appear to originate.
+
+    @returns: str -- external ip address of localhost
+    """
     return requests.get('https://api.ipify.org').text
 
 def parseArgs():
+    """
+    Define and parse command line arguments.
+    """
     parser = argparse.ArgumentParser()
+    parser.description = 'A small script to dynamically update route53 '\
+                'with the public IP address of an EC2 instance.  It can '\
+                'also be run from a host with a dynamic public IP address '\
+                'to update from localhost\'s public IP address.  To run in '\
+                'this mode do not include an instance name/id.'
     parser.add_argument('host', metavar='Hostname',
                 help='The fully qualified domain name to be modified.')
     parser.add_argument('instance', nargs='?', default=None, metavar='Instance',
                 help='Instance name or ID to act as the destination for host.  '\
                     'If instance isn\'t supplied the name is associated with  '\
-                    'the machine running this script.')
+                    'the public IP address of localhost.')
     parser.add_argument('--region', '-r', default='us-east-1', metavar='region',
                 help='region where the instance is running.  (us-east-1)')
     action = parser.add_mutually_exclusive_group()
@@ -101,35 +178,51 @@ def parseArgs():
                 help='If the instance isn\'t running, start it.')
     action.add_argument('--kill', '-k', default=False, action='store_true',
                 help='Don\'t do anything with route53.  Simply stop the instance.')
-    return parser.parse_args()
 
+    args =  parser.parse_args()
 
-def main():
-    args = parseArgs()
+    # route53 requires proper fqdn with '.' termination
     if args.host[-1] != '.':
         args.host += '.'
+
+    # trying to start or stop will no instance is probably an error
     if args.instance is None and (args.start or args.kill):
         print('--start or --kill require an instance.  Exiting.')
         sys.exit(3)
-    match = True
+    return args
 
-    # print('({}, {})'.format(args.host, args.instance))
+
+def procInstance(args):
+    """
+    Process instance and return the public facing IP address.
+    If the --kill command line option is present this function
+    will exit.  It will also exit if no matching Instance could
+    be found.
+
+    @param args: ArgParser args
+
+    @return: str -- IP Address.
+    """
+    ec2Conn = boto.ec2.connect_to_region(args.region)
+    if args.kill:
+        killInstance(ec2Conn, args.instance)
+        sys.exit(0)
+    if args.start:
+        startInstance(ec2Conn, args.instance)
+    return getInstanceIPAddress(ec2Conn, args.instance)
+
+def procHost(args, newIP):
+    """
+    Process host and update route53 records.  Will exit if no
+    appropriate route53 zone could be found.
+
+    @param args: ArgParser args
+
+    @return: None
+    """
+    match = None
     r53Conn = boto.connect_route53()
-
-    if args.instance:
-        ec2Conn = boto.ec2.connect_to_region(args.region)
-        if args.kill:
-            killInstance(ec2Conn, args.instance)
-            sys.exit(0)
-        if args.start:
-            startInstance(ec2Conn, args.instance)
-        newIP = getInstanceIPAddress(ec2Conn, args.instance)
-    else:
-        print('No instance provided, looking up public IP.')
-        newIP = getMyIPAddress()
-    print('IP Address found: {}'.format(newIP))
-
-    zone = getZone(r53Conn, args.host)
+    (zone, match) = getZone(r53Conn, args.host)
     if zone is None:
         print('No appropriate route53 zone could be found for {}'.format(args.host))
         sys.exit(1)
@@ -140,6 +233,22 @@ def main():
     else:
         print('Adding record:  A {} {}'.format(args.host, newIP))
         zone.add_a(args.host, newIP)
+
+def main():
+    """
+    Main part of the script.
+    """
+    args = parseArgs()
+
+    if args.instance:
+        newIP = procInstance(args)
+    else:
+        print('No instance provided, looking up public IP.')
+        newIP = getMyIPAddress()
+    print('IP Address found: {}'.format(newIP))
+
+    procHost(args, newIP)
+
 
 if __name__ == '__main__':
     main()
